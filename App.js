fc4ac5524be754e4e825ab1104e358b8910240de
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-06-04 19:54:56 +0600

    created ViewImageScreen

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 function App() {
-  return <WelcomeScreen />;
+  return (
+    <SafeAreaView style={styles.appContainer}>
+      <ViewImageScreen />
+      {/* <WelcomeScreen /> Uncomment this line and comment the above line to display the WelcomeScreen */}
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight || 20,
+  },
+});
 
 export default App;
---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+
+import colors from "../config/colors";
+
+const ViewImageScreen = () => {
+  return (
+    <View style={styles.imageContainer}>
+      <MaterialCommunityIcons name="email" size={60} color={colors.primary}/>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
