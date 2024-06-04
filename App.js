755db482aa850460b45292a0277a2185ce5403bc
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-06-04 23:08:48 +0600

    Created Product Screen and reusable Card component

--- App.js
+++ App.js
@@ -3,13 +3,16 @@ import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import colors from './app/config/colors';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ProductScreen from './app/screens/ProductScreen';
 
 function App() {
   return (
     <SafeAreaView style={styles.appContainer}>
       {/* <ViewImageScreen /> */}
-      <WelcomeScreen /> 
+      {/* <WelcomeScreen />  */}
       {/* <StyledText style={styles.customText}>This is a custom text</StyledText> */}
+      <ProductScreen/>
+          
     </SafeAreaView>
   );
 }
@@ -18,8 +21,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
     paddingTop: StatusBar.currentHeight || 20,
-    justifyContent: 'center',
-    alignContent: 'center',
+    backgroundColor: '#f8f4f4'
+  
   },
   customText:{
     color: colors.black,
---
+++ app/components/Card.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import AppText from './AppText'
+import colors from '../config/colors'
+
+const Card = ({title, subTitle, image}) => {
+  return (
+    <View style={styles.cardContainer}>
+      <Image source={image} style={styles.image}/>
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    cardContainer: {
+      borderRadius: 15,
+      backgroundColor: colors.white,
+      overflow: "hidden"
+    },
+    image:{
+     width: '100%',
+     height: 250,
+     
+    },
+    detailsContainer:{
+        marginTop: 15,
+        padding: 15,
+        gap:15,
+    },
+    title: {
+        color: colors.black
+    },
+    subTitle:{
+        color: colors.secondary
+    }
+})
+
+export default Card
