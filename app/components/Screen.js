08e47cd68cb44d3b2b5ec23e8c6557b3afc045ab
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-06-05 20:37:24 +0600

    updated screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Platform, SafeAreaView, StatusBar } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 const Screen = ({ children, style = {} }) => {
-  return <SafeAreaView styles={styles}>{children}</SafeAreaView>;
+  return <SafeAreaView styles={[styles, style]}>{children}</SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   padddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  padddingBottom: 20,
 });
 
 export default Screen;
